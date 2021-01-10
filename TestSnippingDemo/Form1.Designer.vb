fcabcf3d3543d9e33e8e4832ec5a50a44de17380
Johnson <sunjunsuz@gmail,com>
2021-01-10 08:04:37 +0800

    first dmeo

---
+++ TestSnippingDemo/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel4 = New System.Windows.Forms.LinkLabel()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(204, Byte), Integer))
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("宋体", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(12, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(84, 56)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "截图"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(102, 17)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(102, 47)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(12, 76)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(190, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.Filter = "*.txt*"
+        Me.FileSystemWatcher1.IncludeSubdirectories = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(27, 236)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(125, 20)
+        Me.TextBox4.TabIndex = 4
+        Me.TextBox4.Text = "D:\\demo199"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 113)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(132, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "站在巨人的肩膀上 VB版"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(15, 176)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(92, 17)
+        Me.RadioButton1.TabIndex = 6
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "120.75 Server"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(125, 176)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(71, 17)
+        Me.RadioButton2.TabIndex = 7
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "16 Server"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(12, 137)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(46, 13)
+        Me.LinkLabel1.TabIndex = 8
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "75 不良"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(77, 137)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(46, 13)
+        Me.LinkLabel2.TabIndex = 9
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "16 不良"
+        '
+        'LinkLabel3
+        '
+        Me.LinkLabel3.AutoSize = True
+        Me.LinkLabel3.Location = New System.Drawing.Point(150, 137)
+        Me.LinkLabel3.Name = "LinkLabel3"
+        Me.LinkLabel3.Size = New System.Drawing.Size(23, 13)
+        Me.LinkLabel3.TabIndex = 10
+        Me.LinkLabel3.TabStop = True
+        Me.LinkLabel3.Text = "ITF"
+        '
+        'LinkLabel4
+        '
+        Me.LinkLabel4.AutoSize = True
+        Me.LinkLabel4.Location = New System.Drawing.Point(190, 137)
+        Me.LinkLabel4.Name = "LinkLabel4"
+        Me.LinkLabel4.Size = New System.Drawing.Size(51, 13)
+        Me.LinkLabel4.TabIndex = 11
+        Me.LinkLabel4.TabStop = True
+        Me.LinkLabel4.Text = "CBackup"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(286, 296)
+        Me.Controls.Add(Me.LinkLabel4)
+        Me.Controls.Add(Me.LinkLabel3)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.TopMost = True
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents LinkLabel4 As System.Windows.Forms.LinkLabel
+    Friend WithEvents LinkLabel3 As System.Windows.Forms.LinkLabel
+    Friend WithEvents LinkLabel2 As System.Windows.Forms.LinkLabel
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+End Class
---
+++ TestSnippingDemo/Form1.vb
@@ -0,0 +1,197 @@
+﻿Imports System.IO
+Imports System.Threading
+
+Public Class Form1
+    '成员变量放置处
+    'Private sw As StreamWriter
+    'Private sr As StreamReader
+    Private Sub WriteContent(Sn As String, PathS As String)
+        Dim fs As New FileStream(PathS + "\\" + "Content.txt", FileMode.Append)
+        Dim sw As New StreamWriter(fs)
+        sw.WriteLine(Sn.ToUpper())
+        sw.Close()
+        fs.Close()
+    End Sub
+
+
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="Sn"></param>
+    ''' <param name="Ma"></param>
+    ''' <param name="Lie"></param>
+    ''' <remarks></remarks>
+    Private Sub SnippingMethods(Sn As String, Ma As String, Lie As String)
+        Me.Hide()
+        Dim p1 As New Point(0, 0)
+        Dim p2 As New Point(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height - 40)
+        Dim pic As New Bitmap(p2.X, p2.Y - 40)
+        Dim parentPath As String = TextBox4.Text
+        Using g As Graphics = Graphics.FromImage(pic)
+            Thread.Sleep(260)
+            g.CopyFromScreen(p1, p1, p2)
+            g.Dispose()
+            'Me.BackgroundImage = pic
+            'pic.Save("D:\\" + Sn + ".jpg")
+            Dim timefolder As String = DateTime.Now.ToString("mmddss")
+            Dim timefolder2 As String = DateTime.Now.ToString("MMdd")
+            Dim dt As DateTime = DateTime.Now
+            Dim dateStr1 As String = "20:00:00"
+            Dim dateStr2 As String = dt.ToString("HH:mm:ss")
+            Dim t1 As DateTime = Convert.ToDateTime(dateStr1)
+            Dim t2 As DateTime = Convert.ToDateTime(dateStr2)
+            Dim compNum As Int16 = DateTime.Compare(t1, t2)
+            If (compNum > 0) Then
+                '继续创建第二天（当天的）的文件夹，并且往里面加上图片
+                Dim saveRealPath As String = parentPath + "\\" + Lie + "\\" + timefolder2 + "\\" + Sn + "__" + Ma + "__" + timefolder + "VB" + ".jpg"
+                Dim filepaht As String = parentPath + "\\" + Lie + "\\" + timefolder2
+                If (Directory.Exists(filepaht)) Then
+                Else
+                    Directory.CreateDirectory(filepaht)
+                End If
+                pic.Save(saveRealPath)
+                WriteContent(Sn, filepaht)
+            ElseIf (compNum < 0) Then
+                '创建第二天的文件夹，并且往里面加上图片
+                Dim tommorm As String = DateTime.Now.AddDays(+1).ToString("MMdd")
+                Dim saveRealPath As String = parentPath + "\\" + Lie + "\\" + tommorm + "\\" + Sn + "__" + Ma + "__" + timefolder + "VB" + ".jpg"
+                Dim filepaht As String = parentPath + "\\" + Lie + "\\" + tommorm
+                If (Directory.Exists(filepaht)) Then
+                Else
+                    Directory.CreateDirectory(filepaht)
+                End If
+                pic.Save(saveRealPath)
+                WriteContent(Sn, filepaht)
+            End If
+        End Using
+        Me.Show()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (TextBox1.Text = "") Then
+            MessageBox.Show("请确框内有值")
+            Return
+        End If
+        If (TextBox2.Text = "") Then
+            MessageBox.Show("请确框内有值")
+            Return
+        End If
+        If (TextBox3.Text = "") Then
+            MessageBox.Show("请确框内有值")
+            Return
+        End If
+        '写入多行数据
+        '以追加的形式写入多行数据
+        'Dim i As Integer
+        'sw = New StreamWriter("D:/21.txt", True, System.Text.Encoding.Default)
+        'sw.WriteLine("test")
+        'sw.Flush()
+        'sw.Close()
+        'sw = Nothing
+        'Dim SnStr As String
+        'SnStr = TextBox1.Text.ToUpper
+        SnippingMethods(TextBox3.Text, TextBox1.Text, TextBox2.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If (Directory.Exists("C:\classifiedDefects\data")) Then
+        Else
+            Directory.CreateDirectory("C:\classifiedDefects\data")
+        End If
+        RadioButton1.Checked = True
+        FileSystemWatcher1.Path = "C:\classifiedDefects\data"
+        'FileSystemWatcher1.Filter = ".txt"
+        TextBox4.Text = "\\172.26.120.75\aoiaxi\AXI\5DX\5DX不良"
+        TextBox1.Text = "V810-8057S2"
+        TextBox2.Text = "R12"
+        TextBox3.Text = "B77880486041900A"
+    End Sub
+
+    Private Sub WriteLineByMachine(maStr As String)
+        If (maStr.Equals("V810-3325S2EX")) Then
+            TextBox2.Text = "Q12"
+        ElseIf (maStr.Equals("V810-3327S2EX")) Then
+            TextBox2.Text = "I12"
+        ElseIf (maStr.Equals("V810-3323S2EX")) Then
+            TextBox2.Text = "I22"
+        ElseIf (maStr.Equals("V810-3328S2EX")) Then
+            TextBox2.Text = "H12"
+        ElseIf (maStr.Equals("V810-8096S2")) Then
+            TextBox2.Text = "J12"
+        ElseIf (maStr.Equals("V810-8085S2")) Then
+            TextBox2.Text = "K12"
+        ElseIf (maStr.Equals("V810-8070S2")) Then
+            TextBox2.Text = "K22"
+        ElseIf (maStr.Equals("V810-8064S2")) Then
+            TextBox2.Text = "L12"
+        ElseIf (maStr.Equals("V810-8057S2")) Then
+            TextBox2.Text = "L22"
+        ElseIf (maStr.Equals("V810-8086S2")) Then
+            TextBox2.Text = "P12"
+        End If
+
+
+    End Sub
+
+    Private Sub FileSystemWatcher1_Created(sender As Object, e As FileSystemEventArgs) Handles FileSystemWatcher1.Created
+        'Dim fs As New FileStream("d:\logs.txt", FileMode.Append)
+        'Dim sw As New StreamWriter(fs)
+        'sw.WriteLine(Now() & Microsoft.VisualBasic.vbTab & "创建" & e.FullPath)
+        'sw.Close()
+        'fs.Close()
+        Dim SnStr1 As String
+        If (e.FullPath.EndsWith("BoardStatus.txt")) Then
+            Thread.Sleep(350)
+            Dim fs1 As New FileStream(e.FullPath, FileMode.Open)
+            Dim rw As New StreamReader(fs1)
+            'Dim b() As String
+            SnStr1 = Split(rw.ReadLine, ";")(1)
+            TextBox3.Text = SnStr1.ToUpper.Trim()
+            Me.Text = SnStr1.ToUpper.Trim()
+            rw.Close()
+            fs1.Close()
+            Dim b() As String
+            Dim nameArr() As String
+            b = Split(e.FullPath, "\")
+            'Dim b2 As String
+            'For Each b3 As String In b
+            'Console.WriteLine(b3)
+            'Next
+            'V810-8085S2[@$@]2020-12-04-21-55-51  3
+            nameArr = Split(b(3), "[")
+            TextBox1.Text = nameArr(0)
+            WriteLineByMachine(nameArr(0))
+            'For Each b4 As String In nameArr
+            'Console.WriteLine(b4)
+            'Next
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        TextBox4.Text = "\\172.26.120.75\aoiaxi\AXI\5DX\5DX不良"
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        TextBox4.Text = "\\172.26.12.16\aoi\5DX\5DX不良"
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Dim path1 As String = "\\172.26.120.75\aoiaxi\AXI\5DX\5DX不良"
+        System.Diagnostics.Process.Start("explorer", path1)
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Dim path1 As String = "\\172.26.12.16\aoi\5DX\5DX不良"
+        System.Diagnostics.Process.Start("explorer", path1)
+    End Sub
+
+    Private Sub LinkLabel3_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        Dim path1 As String = "C:\ITF\XMLCEXPORT"
+        System.Diagnostics.Process.Start("explorer", path1)
+    End Sub
+
+    Private Sub LinkLabel4_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel4.LinkClicked
+        Dim path1 As String = "C:\AXI\CBackup\" + DateTime.Now.ToString("yyyyMMdd")
+        System.Diagnostics.Process.Start("explorer", path1)
+    End Sub
+End Class
